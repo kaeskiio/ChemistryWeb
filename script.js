6b6ea50f98a6069c8b0122b46fbdb54477b97229
kacemettahali <151705697+kacemettahali@users.noreply.github.com>
2024-04-21 02:00:54 -0500

    Try to figure out how to work this

--- script.js
+++ script.js
@@ -1,7 +1,6 @@
-
-
 const questions = [
     {
+        //#1
         question: "What is the name of KNO<sub>3</sub>?",
         answers: [
             { text: "Potassium Nitrate", correct: true},
@@ -11,6 +10,7 @@ const questions = [
         ]
     },
     {
+        //#2
         question: "What is the name of Na<sub>2</sub>SO<sub>4</sub>?",
         answers: [
             { text: "Sodium Nitrate", correct: false},
@@ -19,7 +19,7 @@ const questions = [
             { text: "Sodium Sulfite", correct: false},
         ]
     },
-    {
+    { //#3
         question: "What is the name of CaSO<sub>4</sub>?",
         answers: [
             { text: "Calcium Nitrate", correct: false},
@@ -28,6 +28,160 @@ const questions = [
             { text: "Calcium Sulfite", correct: false},
         ]
     },
+    { //#4
+        question: "What is the formula of Chromium (VI) Phosphate?",
+        answers: [
+            { text: "Cr<sub>3</sub>(PO<sub>4</sub>)", correct: false},
+            { text: "Cr<sub>2</sub>(PO<sub>4</sub>)<sub>3</sub>", correct: false},
+            { text: "Cr(PO<sub>4</sub>)", correct: false},
+            { text: "Cr(PO<sub>4</sub>)<sub>2</sub>", correct: true},
+        ]
+    },
+    { //#5
+        question: "What is the name of CO?",
+        answers: [
+            { text: "Carbon Oxide", correct: false},
+            { text: "Carbonic Oxide", correct: false},
+            { text: "Carbon Monoxide", correct: true},
+            { text: "Monocarbonic Oxide", correct: false},
+        ]
+    },
+    { //#6
+        question: "What is the formula of Magnesium Sulfate Heptahydrate?",
+        answers: [
+            { text: "Mg(SO<sub>4</sub>) * 7H<sub>2</sub>O", correct: true},
+            { text: "Mg(SO<sub>4</sub>) * 6H<sub>2</sub>O", correct: false},
+            { text: "Mg(SO<sub>3</sub>) * 7H<sub>2</sub>O", correct: false},
+            { text: "Mg(SO<sub>3</sub>) * 6H<sub>2</sub>O", correct: false},
+        ]
+    },
+    {  //#7
+        question: "What is the name of CCI<sub>4</sub>?",
+        answers: [
+            { text: "Carbon Chloride", correct: false},
+            { text: "Carbon Tetrachloride", correct: true},
+            { text: "Carbon Chlorate", correct: false},
+            { text: "Carbon Pentachloride", correct: false},
+        ]
+    },
+    { //#8
+        question: "What is the name of V<sub>2</sub>S<sub>3</sub>?",
+        answers: [
+            { text: "Vanadium(III) Sulfide", correct: true },
+            { text: "Vanadium(II) Sulfide", correct: false},
+            { text: "Vanadium(IV) Sulfide", correct: false },
+            { text: "Vanadium(VI) Sulfide", correct: false},
+        ]
+    },
+    { //#99
+        question: "What is the name of SO<sub>2</sub>?" ,
+        answers: [
+            { text: "Sulfuric Acid", correct: false},
+            { text: "Sulfur Monoxide", correct: false},
+            { text: "Sulfur Oxide", correct: false},
+            { text: "Sulfur Dioxide", correct: true},
+        ]
+    },
+    { //#10
+        question: "What is the name of AgC<sub>2</sub>H<sub>3</sub>O<sub>2</sub>?",
+        answers: [
+            { text: "Carbon Oxide", correct: false},
+            { text: "Silver Acetate", correct: true},
+            { text: "Carbon Tetrachloride", correct: false} ,
+            { text: "Monocarbonic Oxide", correct: false},
+        ]
+    },
+    { //#11
+        question: "What is the name of FePO<sub>4</sub>?",
+        answers: [
+            { text: "Iron(II) Phosphate", correct: false},
+            { text: "Iron(VI) Phosphate", correct: false},
+            { text: "Iron(III) Phosphate", correct: true},
+            { text: "Iron(IV) Phosphate", correct: false},
+        ]
+    },
+    {  //#12
+        question: "What is the name of Mn<sub>3</sub>(PO<sub>4</sub>)<sub>2</sub>?",
+        answers: [
+            { text: "Manganese (II) Phosphate", correct: true},
+            { text: "Manganese (VI) Phosphate", correct: false},
+            { text: "Manganese (IV) Phosphate", correct: false} ,
+            { text: "Manganese (III) Phosphate", correct: false},
+        ]
+    },
+    { //#13
+        question: "What is the name of B<sub>2</sub>Br<sub>4</sub>?",
+        answers: [
+            { text: "Boron(IV) Bromide", correct: false},
+            { text: "Tetraboron Octabromide", correct: false},
+            { text: "Diboron Tetrabromide", correct: true},
+            { text: "Boron Bromide", correct: false},
+        ]
+    },
+    { //#14
+        question: "What is the name of SnSe<sub>2</sub>?",
+        answers: [
+            { text: "Tin (IV) Selenide", correct: true},
+            { text: "Tin (II) Selenium", correct: false},
+            { text: "Tin (III) Selenide", correct: false},
+            { text: "Tin (IV) Selenium", correct: false},
+        ]
+    },
+    { //#15
+        question: "What is the name of Ca(C<sub>2</sub>H<sub>3</sub>O<sub>2</sub>)<sub>2</sub>?",
+        answers: [
+            { text: "Calcium Acetite", correct: false},
+            { text: "Calconic Acetate", correct: false},
+            { text: "Acetic Acid", correct: false},
+            { text: "Calcium Acetate", correct: true},
+        ]
+    },
+    { //#16
+        question: "What is the name of ZnS?",
+        answers: [
+            { text: "Zinc Sulfur", correct: false},
+            { text: "Zinc(III) Sulfide", correct: false},
+            { text: "Zinc Sulfide", correct: true},
+            { text: "Zinc Sulfite", correct: false},
+        ]
+    },
+	{ //#17
+        question: "What is the name of CdSO<sub>3</sub>?",
+        answers: [
+            { text: "Cadmium Sulfite", correct: true},
+            { text: "Cadmium Sulfate", correct: false},
+            { text: "Cadmium Sulfide", correct: false},
+            { text: "Cadmium Sulfur", correct: false},
+        ]
+    },
+	{ //#18
+        question: "What is the name of Fe(HCO<sub>3</sub>)<sub>2</sub>?",
+        answers: [
+            { text: "Iron (III) Bicarbonate", correct: false},
+            { text: "Iron (II) Carbonate", correct: false},
+            { text: "Iron (IV) Bicarbonate", correct: false},
+            { text: "Iron (II) Bicarbonate", correct: true},
+        ]
+    },
+	{ //#19
+        question: "What is the name of PbS?",
+        answers: [
+            { text: "Lead (IV) Sulfide", correct: false},
+            { text: "Lead Sulfide", correct: false},
+            { text: "Lead (II) Sulfide", correct: true},
+            { text: "Lead (II) Sulfite", correct: false},
+        ]
+    },
+	{ //#20
+        question: "What is the name of BBr<sub>3</sub>?",
+        answers: [
+            { text: "Boron Tribromide", correct: true},
+            { text: "Boron Bromide", correct: false},
+            { text: "Boron Tribromine", correct: false},
+            { text: "Boronbrominic acid", correct: false},
+        ]
+    },
+	
 ];
 
 const questionElement = document.getElementById("question");
@@ -90,7 +244,13 @@ function selectAnswer(e){
 
 function showScore(){
     resetState();
-    questionElement.innerHTML = `You scored ${score} out of ${questions.length}!`;
+    if(score < questions.length*.7)
+    {
+        questionElement.innerHTML = `You scored ${score} out of ${questions.length}. Play again to improve your score!`;
+    }
+    else{
+        questionElement.innerHTML = `You scored ${score} out of ${questions.length}!`;
+    }
     nextButton.innerHTML = "Play Again";
     nextButton.style.display = "block";
 }
